e321013da375b51d8d90a13b4428a08df59b471c
dllo <dllo@dllodeMac-mini-273.local>
2017-08-05 16:20:42 +0800

    周六作业

---
+++ 8月5日/routes/loginSuccess.js
@@ -0,0 +1,11 @@
+/**
+ * Created by dllo on 17/8/5.
+ */
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('loginSuccess');
+});
+
+module.exports = router;
